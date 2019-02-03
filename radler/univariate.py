7a6a13e6c4254fe490f951fe72c38af2f54ec307
Hubert Gabryś <hubert.gabrys@gmail.com>
2019-02-03 12:08:28 +0100

    Support for DataFrame int column names in plot_roc_curve()

--- radler/univariate.py
+++ radler/univariate.py
@@ -64,7 +64,7 @@ def plot_roc_curve(df, column, y):
     roc_auc = auc(fpr, tpr)
     plt.figure()
     lw = 2
-    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='AUC(' + column + ') = %0.2f' % roc_auc)
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='AUC({}) = {:.2f}'.format(column, roc_auc))
     plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
